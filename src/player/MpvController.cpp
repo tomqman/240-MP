243aaabffefd4db669a5e03986ed9e4c070d81e2
tomqman <45924099+tomqman@users.noreply.github.com>
2026-06-14 08:57:11 +0100

    Update MpvController.cpp
    adjusted media controls

--- src/player/MpvController.cpp
+++ src/player/MpvController.cpp
@@ -53,17 +53,37 @@ MpvController::MpvController(const QString &appRoot, QObject *parent)
         f.write("ESC quit\n");
         f.write("BS quit\n");
         f.write("ENTER cycle pause\n");
-        f.write("XF86AudioPlay cycle pause\n");
-        f.write("XF86AudioPause cycle pause\n");
-        f.write("KEY_PLAYPAUSE cycle pause\n");
-        f.write("XF86AudioStop stop\n");
-        f.write("XF86AudioNext playlist-next\n");
-        f.write("XF86AudioPrev playlist-prev\n");
-        f.write("XF86AudioRaiseVolume add volume 5\n");
-        f.write("XF86AudioLowerVolume add volume -5\n");
-        f.write("XF86AudioMute cycle mute\n");
+
+        bool headless = detectHeadlessMode();
+        if (headless) {
+            // evdev key names used when running without X11/Wayland
+            f.write("KEY_PLAYPAUSE cycle pause\n");
+            f.write("KEY_PLAY cycle pause\n");
+            f.write("KEY_PAUSE cycle pause\n");
+            f.write("KEY_STOPCD stop\n");
+            f.write("KEY_STOP stop\n");
+            f.write("KEY_NEXTSONG playlist-next\n");
+            f.write("KEY_NEXT playlist-next\n");
+            f.write("KEY_PREVIOUSSONG playlist-prev\n");
+            f.write("KEY_PREVIOUS playlist-prev\n");
+            f.write("KEY_VOLUMEUP add volume 5\n");
+            f.write("KEY_VOLUMEDOWN add volume -5\n");
+            f.write("KEY_MUTE cycle mute\n");
+        } else {
+            // X11/Wayland key names
+            f.write("XF86AudioPlay cycle pause\n");
+            f.write("XF86AudioPause cycle pause\n");
+            f.write("XF86AudioStop stop\n");
+            f.write("XF86AudioNext playlist-next\n");
+            f.write("XF86AudioPrev playlist-prev\n");
+            f.write("XF86AudioRaiseVolume add volume 5\n");
+            f.write("XF86AudioLowerVolume add volume -5\n");
+            f.write("XF86AudioMute cycle mute\n");
+        }
+
         f.close();
     }
+
 
     m_ipc = new QLocalSocket(this);
     connect(m_ipc, &QLocalSocket::connected, this, [this] {
